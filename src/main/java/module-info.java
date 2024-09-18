65ff957a0f00474356fb5b2dc44fd88ebb78e5d0
Juhyun Park <jpark440@my.bcit.ca>
2024-09-17 23:10:34 -0700

    Hello World!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.comp2522202430termprojectpersianrug {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.comp2522202430termprojectpersianrug to javafx.fxml;
+    exports com.example.comp2522202430termprojectpersianrug;
+}
