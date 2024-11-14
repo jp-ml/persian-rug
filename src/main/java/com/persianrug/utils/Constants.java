4967198841b5b3109609fa5529c00b230436b3a7
Juhyun Park <jpark440@my.bcit.ca>
2024-11-14 00:01:17 -0800

    Add Constant values file

---
+++ src/main/java/com/persianrug/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.persianrug.utils;
+
+public class Constants {
+    public static final int WINDOW_WIDTH = 1200;
+    public static final int WINDOW_HEIGHT = 800;
+
+    public static final double PLAYER_SPEED = 5.0;
+    public static final double GRAVITY = 0.5;
+    public static final double JUMP_FORCE = -15.0;
+    public static final double FRICTION = 0.8;
+
+    public static final double PLAYER_WIDTH = 50;
+    public static final double PLAYER_HEIGHT = 30;
+}
