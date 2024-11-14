cfa727695871585d0ce4e3da3d59ad41c309dd6c
Juhyun Park <jpark440@my.bcit.ca>
2024-11-14 00:02:48 -0800

    Add game object related files

---
+++ src/main/java/com/persianrug/entity/GameObject.java
@@ -0,0 +1,24 @@
+package com.persianrug.entity;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public abstract class GameObject {
+    protected double x, y;
+    protected double width, height;
+
+    public GameObject(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void update();
+    public abstract void render(GraphicsContext gc);
+
+    // Getters
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
+}
