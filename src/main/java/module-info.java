9c617dc7ebf52fe2aa018b21d4190a2cc5c72019
Juhyun Park <jpark440@my.bcit.ca>
2024-11-14 00:04:12 -0800

    Add configuration files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,23 @@
-module com.example.comp2522202430termprojectpersianrug {
+module com.persianrug {
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Main package
+    opens com.persianrug to javafx.fxml;
+    exports com.persianrug;
 
-    opens com.example.comp2522202430termprojectpersianrug to javafx.fxml;
-    exports com.example.comp2522202430termprojectpersianrug;
+    // Engine package
+    opens com.persianrug.engine to javafx.fxml;
+    exports com.persianrug.engine;
+
+    // Entity package
+    opens com.persianrug.entity to javafx.fxml;
+    exports com.persianrug.entity;
+
+    // Utils package
+    opens com.persianrug.utils to javafx.fxml;
+    exports com.persianrug.utils;
+
+    // Image resources
+    opens images;
 }
