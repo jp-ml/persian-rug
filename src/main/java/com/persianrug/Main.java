8c2e2da2c76e944423c46be89b2edcde67e18389
Juhyun Park <jpark440@my.bcit.ca>
2024-11-30 21:04:32 -0800

    Update the Main Class to satisfy all the checkstyle warnings

--- src/main/java/com/persianrug/Main.java
+++ src/main/java/com/persianrug/Main.java
@@ -4,22 +4,45 @@ import com.persianrug.engine.GameEngine;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+/**
+ * The entry point for the JavaFX application.
+ * This class is final to prevent unintended extension.
+ *
+ * @author Juhyun Park
+ * @version 2024
+ */
+public final class Main extends Application {
+
     private GameEngine gameEngine;
 
+    /**
+     * Starts the JavaFX application.
+     *
+     * @param primaryStage the primary stage for this application, must not be null
+     */
     @Override
-    public void start(Stage primaryStage) {
+    public void start(final Stage primaryStage) {
         gameEngine = new GameEngine(primaryStage);
         gameEngine.start();
         primaryStage.show();
     }
 
+    /**
+     * Stops the JavaFX application and cleans up resources.
+     */
     @Override
     public void stop() {
-        gameEngine.stop();
+        if (gameEngine != null) {
+            gameEngine.stop();
+        }
     }
 
-    public static void main(String[] args) {
+    /**
+     * The main method serves as the entry point for the application.
+     *
+     * @param args command-line arguments passed to the application, must not be null
+     */
+    public static void main(final String[] args) {
         launch(args);
     }
 }
