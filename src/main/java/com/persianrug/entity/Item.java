b686a0f397c51578c02c099863da9503da8830f4
Homayoun Khoshi <Khoshihomayoun@gmail.com>
2024-11-30 16:01:36 -0800

    Update the item class to satisfy all the checkstyle warnings

--- src/main/java/com/persianrug/entity/Item.java
+++ src/main/java/com/persianrug/entity/Item.java
@@ -5,23 +5,39 @@ import javafx.scene.image.Image;
 
 import java.net.URL;
 
+/**
+ * Represents an item in the game that can be collected by the player.
+ * Each item is associated with a quiz and has an image representation.
+ * @author Juhyun Park
+ * @version 2024
+ */
 public class Item extends GameObject {
+    private static final int ITEM_SIZE = 50;
     private Image itemImage;
     private boolean collected = false;
     private final Quiz quiz;
 
-    public Item() {
-        super(0, 0, 50, 50);
-        this.quiz = null;
-    }
-
-    public Item(double x, double y, String imagePath, Quiz quiz) {
-        super(x, y, 50, 50);
+    /**
+     * Constructs an Item object with specified position, image, and associated quiz.
+     *
+     * @param x         the x-coordinate of the item's position.
+     * @param y         the y-coordinate of the item's position.
+     * @param imagePath the relative path to the image representing the item.
+     * @param quiz      the quiz associated with the item.
+     */
+    public Item(final double x, final double y, final String imagePath, final Quiz quiz) {
+        super(x, y, ITEM_SIZE, ITEM_SIZE);
         this.quiz = quiz;
         loadImage(imagePath);
     }
 
-    private void loadImage(String imagePath) {
+    /**
+     * Loads the image for the item from the specified path.
+     *
+     * @param imagePath the relative path to the image file.
+     * @throws RuntimeException if the image resource cannot be found.
+     */
+    private void loadImage(final String imagePath) {
         URL resourceUrl = getClass().getResource(imagePath);
         if (resourceUrl == null) {
             throw new RuntimeException("Resource not found: " + imagePath);
@@ -29,31 +45,48 @@ public class Item extends GameObject {
         itemImage = new Image(resourceUrl.toString());
     }
 
+    /**
+     * Updates the item's state. Currently, this method is a placeholder.
+     */
     @Override
     public void update() {
     }
 
+    /**
+     * Renders the item on the given GraphicsContext if it has not been collected.
+     *
+     * @param gc the GraphicsContext to render the item on.
+     */
     @Override
-    public void render(GraphicsContext gc) {
+    public void render(final GraphicsContext gc) {
         if (!collected && itemImage != null) {
             gc.drawImage(itemImage, x, y, width, height);
         }
     }
 
+    /**
+     * Checks if the item has been collected.
+     *
+     * @return {@code true} if the item has been collected, {@code false} otherwise.
+     */
     public boolean isCollected() {
         return collected;
     }
 
+    /**
+     * Marks the item as collected and logs the collection event.
+     */
     public void collect() {
         this.collected = true;
         System.out.println("Item collected!");
     }
 
+    /**
+     * Retrieves the quiz associated with this item.
+     *
+     * @return the associated Quiz object.
+     */
     public Quiz getQuiz() {
         return quiz;
-    }
-
-    public void setCollected(boolean collected) {
-        this.collected = collected;
     }
 }
