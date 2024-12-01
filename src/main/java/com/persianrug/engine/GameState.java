aad296ca29d1017c8aa9d25d3af9a99efdb2f31b
Juhyun Park <jpark440@my.bcit.ca>
2024-11-30 20:15:48 -0800

    Update the GameState file to satisfy all the checkstyle warnings

--- src/main/java/com/persianrug/engine/GameState.java
+++ src/main/java/com/persianrug/engine/GameState.java
@@ -1,8 +1,30 @@
 package com.persianrug.engine;
 
+/**
+ * Represents the various states the game can be in.
+ * This enum helps in controlling the game's flow and state transitions.
+ *
+ * @author Homayoun Khoshi
+ * @version 2024
+ */
 public enum GameState {
+    /**
+     * Represents the main menu state.
+     */
     MENU,
+
+    /**
+     * Represents the state where the game is actively being played.
+     */
     PLAYING,
+
+    /**
+     * Represents the state where the game is paused.
+     */
     PAUSED,
+
+    /**
+     * Represents the state where the game is saving progress.
+     */
     SAVE_GAME
 }
